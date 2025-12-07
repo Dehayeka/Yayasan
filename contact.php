05a1a4aa0c1d459b46cca2ac71dd105c376f6bb4
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2025-12-07 14:41:22 +0700

    maps

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@
 
   <title>Yayasan Dharma Laksana</title>
 
+  <!-- Mobile Specific Meta-->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- bootstrap.min css -->
@@ -29,41 +30,40 @@
 <body id="top-header">
 
 <header>
-  <div class="site-navigation main_menu menu-transparent" id="mainmenu-area">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid container-padding">
-        <a class="navbar-brand" href="index.html">
-          <img src="assets/images/dark-logo.png" alt="Eduhash" class="img-fluid">
-        </a>
+    <div class="site-navigation main_menu menu-transparent" id="mainmenu-area">
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid container-padding">
+                <a class="navbar-brand" href="index.html">
+                    <img src="assets/images/dark-logo.png" alt="Eduhash" class="img-fluid">
+                </a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu"
-          aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="fa fa-bars"></span>
-        </button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
+                    <span class="fa fa-bars"></span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbarMenu">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item"><a href="index.html" class="nav-link">Dashboard</a></li>
-            <li class="nav-item"><a href="index-2.html" class="nav-link">Visi Misi</a></li>
-            <li class="nav-item"><a href="about.html" class="nav-link">Tentang Kami</a></li>
-            <li class="nav-item"><a href="blog.html" class="nav-link">Kegiatan Kami</a></li>
-            <li class="nav-item"><a href="contact.html" class="nav-link" style="color:#20ad96;">Hubungi Kami</a></li>
-          </ul>
+                <div class="collapse navbar-collapse" id="navbarMenu">
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item"><a href="index.html" class="nav-link">Dashboard</a></li>
+                        <li class="nav-item"><a href="index-2.html" class="nav-link">Visi Misi</a></li>
+                        <li class="nav-item"><a href="about.html" class="nav-link">Tentang Kami</a></li>
+                        <li class="nav-item"><a href="blog.html" class="nav-link">Kegiatan Kami</a></li>
+                        <li class="nav-item"><a href="contact.html" class="nav-link" style="color:#20ad96;">Hubungi Kami</a></li>
+                    </ul>
 
-          <div class="d-flex align-items-center">
-            <div class="header-socials social-links d-none d-lg-none d-xl-block">
-              <a href="#"><i class="fab fa-facebook-f"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-linkedin"></i></a>
-              <a href="#"><i class="fab fa-pinterest"></i></a>
+                    <div class="d-flex align-items-center">
+                        <div class="header-socials social-links d-none d-lg-none d-xl-block">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-twitter"></i></a>
+                            <a href="#"><i class="fab fa-linkedin"></i></a>
+                            <a href="#"><i class="fab fa-pinterest"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-
-        </div>
-      </div>
-    </nav>
-  </div>
+        </nav>
+    </div>
 </header>
+
 
 <section class="page-header">
   <div class="container">
@@ -82,123 +82,149 @@
   </div>
 </section>
 
-<!-- Map Section -->
+
+<!-- MAP FIX TANPA API KEY -->
 <section class="map">
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12">
+   <div class="container">
+       <div class="row">
+           <div class="col-xl-12">
                 <div style="width:100%; height:450px;">
                     <iframe
-                        width="100%"
-                        height="100%"
-                        style="border:0;"
-                        loading="lazy"
-                        allowfullscreen
-                        referrerpolicy="no-referrer-when-downgrade"
-                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDUMMYKEY
-                            &q=-8.6150363,116.1228922
-                            &zoom=16">
+                      width="100%"
+                      height="100%"
+                      style="border:0"
+                      loading="lazy"
+                      allowfullscreen
+                      referrerpolicy="no-referrer-when-downgrade"
+                      src="https://www.google.com/maps?q=-8.6150363,116.1228922&z=17&output=embed">
                     </iframe>
+                </div>
+           </div>
+       </div>
+   </div>
+</section>
+<!-- MAP END -->
+
+
+<section class="contact section-padding">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-7 col-xl-7">
+                <div class="section-heading center-heading">
+                    <span class="subheading">contact</span>
+                    <h3>For more information about our courses, get in touch</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+
+            <div class="col-lg-4">
+                <div class="row">
+                    <div class="col-lg-12 col-md-6">
+                        <div class="contact-item">
+                            <p>Email Us</p>
+                            <h4>xxxxx@gmail.com</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-6">
+                        <div class="contact-item">
+                            <p>Phone</p>
+                            <h4>+62 xxxxxxxxx</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-6">
+                        <div class="contact-item">
+                            <p>Office</p>
+                            <h4>Jl. Dr. Sujono No. 15, Mataram, NTB</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-8">
+                <form class="contact__form form-row" method="post" action="mail.php" id="contactForm">
+
+                   <div class="row">
+                       <div class="col-12">
+                           <div class="alert alert-success contact__msg" style="display:none">
+                               Your message was sent successfully.
+                           </div>
+                       </div>
+                   </div>
+
+                   <div class="row">
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <input type="text" name="name" class="form-control" placeholder="Your Name">
+                           </div>
+                       </div>
+
+                       <div class="col-lg-6">
+                           <div class="form-group">
+                               <input type="text" name="email" class="form-control" placeholder="Email Address">
+                           </div>
+                       </div>
+
+                       <div class="col-lg-12">
+                           <div class="form-group">
+                               <input type="text" name="subject" class="form-control" placeholder="Subject">
+                           </div>
+                       </div>
+
+                       <div class="col-lg-12">
+                           <div class="form-group">
+                               <textarea name="message" cols="30" rows="6" class="form-control" placeholder="Your Message"></textarea>
+                           </div>
+                       </div>
+                   </div>
+
+                   <div class="col-lg-12">
+                       <div class="mt-4">
+                           <button class="btn btn-main" type="submit">Send Message</button>
+                       </div>
+                   </div>
+               </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="footer-2">
+    <div class="container"></div>
+        <div class="row justify-content-center align-items-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="widget footer-about mb-4 text-center">
+                    <h5 class="widget-title text-gray">About us</h5>
+                    <ul class="list-unstyled footer-info">
+                        <li><span>Ph:</span> +62 xxxxxxxxx</li>
+                        <li><span>Email:</span> xxxxx@gmail.com</li>
+                        <li><span>Location:</span> Jl. Dr. Sujono No. 15, Mataram, NTB</li>
+                    </ul>
+                    <ul class="list-inline footer-socials">
+                        <li class="list-inline-item">Follow us :</li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-<!-- Contact Section -->
-<section class="contact section-padding">
-  <div class="container">
-    <div class="row align-items-center justify-content-center">
-      <div class="col-lg-7 col-xl-7">
-        <div class="section-heading center-heading">
-          <span class="subheading">Contact</span>
-          <h3>For more information about our programs, get in touch</h3>
-        </div>
-      </div>
-    </div>
-
-    <div class="row justify-content-center">
-      <div class="col-lg-4">
-        <div class="row">
-          <div class="col-lg-12 col-md-6">
-            <div class="contact-item">
-              <p>Email Us</p>
-              <h4>xxxxx@gmail.com</h4>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-6">
-            <div class="contact-item">
-              <p>Make a Call</p>
-              <h4>+62 xxxxxxxx</h4>
-            </div>
-          </div>
-          <div class="col-lg-12 col-md-6">
-            <div class="contact-item">
-              <p>Office</p>
-              <h4>Jl. Dr. Sujono No. 15, Mataram, Lombok, NTB</h4>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-8">
-        <form class="contact__form form-row" method="post" action="mail.php" id="contactForm">
-
-          <div class="row">
-            <div class="col-12">
-              <div class="alert alert-success contact__msg" style="display:none">
-                Your message was sent successfully.
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-lg-6"><input type="text" name="name" class="form-control" placeholder="Your Name"></div>
-            <div class="col-lg-6"><input type="email" name="email" class="form-control" placeholder="Email Address"></div>
-            <div class="col-lg-12"><input type="text" name="subject" class="form-control" placeholder="Subject"></div>
-            <div class="col-lg-12"><textarea name="message" rows="6" class="form-control" placeholder="Your Message"></textarea></div>
-          </div>
-
-          <div class="col-lg-12 mt-4">
-            <button class="btn btn-main" type="submit">Send Message</button>
-          </div>
-
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-
-<!-- Footer -->
-<section class="footer-2">
-  <div class="container">
-    <div class="row justify-content-center align-items-center">
-      <div class="col-lg-6 col-md-8">
-        <div class="widget footer-about mb-4 text-center">
-          <h5 class="widget-title text-gray">About us</h5>
-          <ul class="list-unstyled footer-info">
-            <li><span>Ph:</span> +62 xxxxxxxxx</li>
-            <li><span>Email:</span> xxxxx@gmail.com</li>
-            <li><span>Location:</span> Jl. Dr. Sujono No. 15, Mataram, Lombok</li>
-          </ul>
-          <ul class="list-inline footer-socials">
-            <li class="list-inline-item">Follow us :</li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-            <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
 <div class="fixed-btm-top">
-  <a href="#top-header" class="js-scroll-trigger scroll-to-top"><i class="fa fa-angle-up"></i></a>
+    <a href="#top-header" class="js-scroll-trigger scroll-to-top">
+        <i class="fa fa-angle-up"></i>
+    </a>
 </div>
 
-<!-- Scripts -->
+
+<!-- SCRIPTS -->
 <script src="assets/vendors/jquery/jquery.js"></script>
 <script src="assets/vendors/bootstrap/bootstrap.js"></script>
 <script src="assets/vendors/counterup/waypoint.js"></script>
@@ -206,9 +232,6 @@
 <script src="assets/vendors/jquery.isotope.js"></script>
 <script src="assets/vendors/imagesloaded.js"></script>
 <script src="assets/vendors/owl/owl.carousel.min.js"></script>
-
-<!-- map.js + API KEY REMOVED -->
-
 <script src="assets/js/script.js"></script>
 
 </body>
