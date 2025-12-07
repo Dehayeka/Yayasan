62bbce6f79fd03db30e2f4db69d1370085edf565
Adhyatmika Eka Saputra <148543848+Dehayeka@users.noreply.github.com>
2025-12-07 14:35:09 +0700

    maps

--- contact.php
+++ contact.php
@@ -1,287 +1,213 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="description" content="Education LMS template by Dreambuzz">
-  <meta name="keywords" content="Eduhash,education,lms,seo,course,online,learning,caoch,training,tutor">
-  <meta name="author" content="themeturn.com">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Education LMS template by Dreambuzz">
+  <meta name="keywords" content="Eduhash,education,lms,seo,course,online,learning,caoch,training,tutor">
+  <meta name="author" content="themeturn.com">
 
-  <title>Yayasan Dharma Laksana</title>
+  <title>Yayasan Dharma Laksana</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="assets/vendors/bootstrap/bootstrap.css">
-    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.css">
-  <link rel="stylesheet" href="assets/vendors/flaticon/flaticon.css">
-    <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
-  <link rel="stylesheet" href="assets/vendors/owl/assets/owl.carousel.min.css">
-  <link rel="stylesheet" href="assets/vendors/owl/assets/owl.theme.default.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/responsive.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="assets/vendors/bootstrap/bootstrap.css">
+  <!-- Iconfont Css -->
+  <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.css">
+  <link rel="stylesheet" href="assets/vendors/flaticon/flaticon.css">
+  <!-- animate.css -->
+  <link rel="stylesheet" href="assets/vendors/animate-css/animate.css">
+  <link rel="stylesheet" href="assets/vendors/owl/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="assets/vendors/owl/assets/owl.theme.default.min.css">
 
-  <style>
-    /* Menentukan tinggi untuk div peta agar dapat ditampilkan */
-    #map {
-        height: 400px; 
-        width: 100%;
-        background-color: #eee; /* Untuk visual saat peta belum dimuat */
-    }
-  </style>
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/responsive.css">
 
 </head>
 
 <body id="top-header">
 
+<header>
+  <div class="site-navigation main_menu menu-transparent" id="mainmenu-area">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid container-padding">
+        <a class="navbar-brand" href="index.html">
+          <img src="assets/images/dark-logo.png" alt="Eduhash" class="img-fluid">
+        </a>
 
-    
-    <header> 
-   
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu"
+          aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fa fa-bars"></span>
+        </button>
 
-               
-        <div class="site-navigation main_menu menu-transparent" id="mainmenu-area">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container-fluid container-padding">
-                    <a class="navbar-brand" href="index.html">
-                        <img src="assets/images/dark-logo.png" alt="Eduhash" class="img-fluid">
-                    </a>
-    
-                        
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="fa fa-bars"></span>
-                    </button>
-    
-                                        <div class="collapse navbar-collapse" id="navbarMenu">
-                       
-                        <ul class="navbar-nav mx-auto">
-                            
-                            <li class="nav-item ">
-                                <a href="index.html" class="nav-link js-scroll-trigger">
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="index-2.html" class="nav-link js-scroll-trigger">
-                                    Visi Misi
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="about.html" class="nav-link js-scroll-trigger">
-                                    Tentang Kami
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="blog.html" class="nav-link js-scroll-trigger" >
-                                    Kegiatan Kami
-                                </a>
-                            </li>
-                            <li class="nav-item ">
-                                <a href="contact.html" class="nav-link js-scroll-trigger" style="color: #20ad96;">
-                                    Hubungi Kami
-                                </a>
-                            </li>
-    
-                            
-                        
-                        <div class="d-flex align-items-center">
-                            <div class="header-socials social-links d-none d-lg-none d-xl-block">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="#"><i class="fab fa-linkedin"></i></a>
-                                <a href="#"><i class="fab fa-pinterest"></i></a>
-                            </div>
-        
-                            
-                        </div>
-                       
-                    </div>                 </div>             </nav>
-        </div>
-    </header>
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item"><a href="index.html" class="nav-link">Dashboard</a></li>
+            <li class="nav-item"><a href="index-2.html" class="nav-link">Visi Misi</a></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">Tentang Kami</a></li>
+            <li class="nav-item"><a href="blog.html" class="nav-link">Kegiatan Kami</a></li>
+            <li class="nav-item"><a href="contact.html" class="nav-link" style="color:#20ad96;">Hubungi Kami</a></li>
+          </ul>
 
+          <div class="d-flex align-items-center">
+            <div class="header-socials social-links d-none d-lg-none d-xl-block">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-linkedin"></i></a>
+              <a href="#"><i class="fab fa-pinterest"></i></a>
+            </div>
+          </div>
 
-        
-
-
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>
 
 <section class="page-header">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-8 col-xl-8">
-        <div class="title-block">
-          <h1>Contact Us</h1>
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item">
-              <a href="#">Home</a>
-            </li>
-             <li class="list-inline-item">/</li>
-            <li class="list-inline-item">
-                Contact
-            </li>
-          </ul>
-        </div>
-        
-
-[Image of a contact page with a map and contact form]
-
-      </div>
-    </div>
-  </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-8 col-xl-8">
+        <div class="title-block">
+          <h1>Contact Us</h1>
+          <ul class="list-inline mb-0">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item">/</li>
+            <li class="list-inline-item">Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
 
+<!-- Map Section -->
 <section class="map">
-   <div class="container">
-       <div class="row">
-           <div class="col-xl-12">
-                <div **id="map"**></div> 
-           </div>
-       </div>
-   </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-12">
+        <div style="width:100%; height:450px;">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.1824105292823!2d116.40855977492284!3d-8.530501687427267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdb4dfeb2d4c03%3A0xc7daa5ea18284d88!2sSembalun%20Lombok!5e0!3m2!1sen!2sid!4v1709200000000"
+            width="100%" 
+            height="100%" 
+            style="border:0;" 
+            allowfullscreen="" 
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
+
+<!-- Contact Section -->
 <section class="contact section-padding">
-    <div class="container">
-        <div class="row align-items-center justify-content-center">
-            <div class="col-lg-7 col-xl-7">
-                <div class="section-heading center-heading">
-                    <span class="subheading">contact</span>
-                    <h3>For more information about our courses, get in touch</h3>
-                </div>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row align-items-center justify-content-center">
+      <div class="col-lg-7 col-xl-7">
+        <div class="section-heading center-heading">
+          <span class="subheading">Contact</span>
+          <h3>For more information about our programs, get in touch</h3>
+        </div>
+      </div>
+    </div>
 
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <div class="row">
-                    <div class="col-lg-12 col-md-6">
-                        <div class="contact-item">
-                            <p>Email Us</p>
-                            <h4>support@email.com</h4>
-                        </div>
-                    </div>
-                     <div class="col-lg-12 col-md-6">
-                        <div class="contact-item">
-                            <p>Make a Call</p>
-                            <h4>+45 234 345467</h4>
-                        </div>
-                    </div>
-                     <div class="col-lg-12 col-md-6">
-                        <div class="contact-item">
-                            <p>Corporate Office</p>
-                            <h4>Moon Street Light Avenue, 14/05
-                                Jupiter, JP 80630 </h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-8">
-                <form class="contact__form form-row " method="post" action="mail.php" id="contactForm">
-                    <div class="row">
-                       <div class="col-12">
-                           <div class="alert alert-success contact__msg" style="display: none" role="alert">
-                               Your message was sent successfully.
-                           </div>
-                       </div>
-                   </div>
+    <div class="row justify-content-center">
+      <div class="col-lg-4">
+        <div class="row">
+          <div class="col-lg-12 col-md-6">
+            <div class="contact-item">
+              <p>Email Us</p>
+              <h4>xxxxx@gmail.com</h4>
+            </div>
+          </div>
+          <div class="col-lg-12 col-md-6">
+            <div class="contact-item">
+              <p>Make a Call</p>
+              <h4>+62 xxxxxxxx</h4>
+            </div>
+          </div>
+          <div class="col-lg-12 col-md-6">
+            <div class="contact-item">
+              <p>Office</p>
+              <h4>Jl. Dr. Sujono No. 15, Mataram, Lombok, NTB</h4>
+            </div>
+          </div>
+        </div>
+      </div>
 
-                   <div class="row">
-                       <div class="col-lg-6">
-                           <div class="form-group">
-                               <input type="text" id="name" name="name" class="form-control" placeholder="Your Name">
-                           </div>
-                       </div>
-                       
-                       <div class="col-lg-6">
-                           <div class="form-group">
-                               <input type="text" name="email" id="email" class="form-control" placeholder="Email Address">
-                           </div>
-                       </div>
-                       <div class="col-lg-12">
-                           <div class="form-group">
-                               <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject">
-                           </div>
-                       </div>
-                       
-                       <div class="col-lg-12">
-                           <div class="form-group">
-                               <textarea id="message" name="message" cols="30" rows="6" class="form-control" placeholder="Your Message"></textarea>    
-                           </div>
-                       </div>
-                   </div>
+      <div class="col-lg-8">
+        <form class="contact__form form-row" method="post" action="mail.php" id="contactForm">
 
-                   <div class="col-lg-12">
-                       <div class="mt-4 ">
-                           <button class="btn btn-main" type="submit">Send Message</button>
-                       </div>
-                   </div>
-               </form> 
-            </div>
-        </div>
-      </div>
+          <div class="row">
+            <div class="col-12">
+              <div class="alert alert-success contact__msg" style="display:none">
+                Your message was sent successfully.
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6"><input type="text" name="name" class="form-control" placeholder="Your Name"></div>
+            <div class="col-lg-6"><input type="email" name="email" class="form-control" placeholder="Email Address"></div>
+            <div class="col-lg-12"><input type="text" name="subject" class="form-control" placeholder="Subject"></div>
+            <div class="col-lg-12"><textarea name="message" rows="6" class="form-control" placeholder="Your Message"></textarea></div>
+          </div>
+
+          <div class="col-lg-12 mt-4">
+            <button class="btn btn-main" type="submit">Send Message</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
 </section>
 
+<!-- Footer -->
 <section class="footer-2">
-    <div class="container"></div>
-        <div class="row justify-content-center align-items-center">
-            <div class="col-lg-6 col-md-8">
-                <div class="widget footer-about mb-4 text-center">
-                    <h5 class="widget-title text-gray">About us</h5>
-                    <ul class="list-unstyled footer-info">
-                        <li><span>Ph:</span>+(62) xxxxxxxxxx</li>
-                        <li><span>Email:</span>xxxxx@gmail.com</li>
-                        <li><span>Location:</span> Jl. Dr. Sujono, No. 15, Lingkar Selatan, Mataram, Lombok, Nusa Tenggara Barat</li>
-                    </ul>
-                    <ul class="list-inline footer-socials">
-                        <li class="list-inline-item">Follow us :</li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li class="list-inline-item"> <a href="#"><i class="fab fa-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                        <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="container">
+    <div class="row justify-content-center align-items-center">
+      <div class="col-lg-6 col-md-8">
+        <div class="widget footer-about mb-4 text-center">
+          <h5 class="widget-title text-gray">About us</h5>
+          <ul class="list-unstyled footer-info">
+            <li><span>Ph:</span> +62 xxxxxxxxx</li>
+            <li><span>Email:</span> xxxxx@gmail.com</li>
+            <li><span>Location:</span> Jl. Dr. Sujono No. 15, Mataram, Lombok</li>
+          </ul>
+          <ul class="list-inline footer-socials">
+            <li class="list-inline-item">Follow us :</li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+            <li class="list-inline-item"><a href="#"><i class="fab fa-pinterest"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
+
 <div class="fixed-btm-top">
-    <a href="#top-header" class="js-scroll-trigger scroll-to-top"><i class="fa fa-angle-up"></i></a>
+  <a href="#top-header" class="js-scroll-trigger scroll-to-top"><i class="fa fa-angle-up"></i></a>
 </div>
 
+<!-- Scripts -->
+<script src="assets/vendors/jquery/jquery.js"></script>
+<script src="assets/vendors/bootstrap/bootstrap.js"></script>
+<script src="assets/vendors/counterup/waypoint.js"></script>
+<script src="assets/vendors/counterup/jquery.counterup.min.js"></script>
+<script src="assets/vendors/jquery.isotope.js"></script>
+<script src="assets/vendors/imagesloaded.js"></script>
+<script src="assets/vendors/owl/owl.carousel.min.js"></script>
 
+<!-- map.js + API KEY REMOVED -->
 
+<script src="assets/js/script.js"></script>
 
-        
-        <script src="assets/vendors/jquery/jquery.js"></script>
-        <script src="assets/vendors/bootstrap/bootstrap.js"></script>
-        <script src="assets/vendors/counterup/waypoint.js"></script>
-    <script src="assets/vendors/counterup/jquery.counterup.min.js"></script>
-    <script src="assets/vendors/jquery.isotope.js"></script>
-    <script src="assets/vendors/imagesloaded.js"></script>
-    <script src="assets/vendors/owl/owl.carousel.min.js"></script>
-    
-    ****
-    <script src="assets/vendors/google-map/map.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>
-    
-    ****
-    <script>
-        var map;
-        function initMap() {
-            // Koordinat untuk "Jl. Dr. Sujono, No. 15, Lingkar Selatan, Mataram"
-            var mataramOffice = {lat: -8.5901, lng: 116.1158}; 
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: mataramOffice, 
-                zoom: 15 // Level zoom yang baik untuk lokasi kantor
-            });
-
-            var marker = new google.maps.Marker({
-                position: mataramOffice,
-                map: map,
-                title: 'Yayasan Dharma Laksana Mataram'
-            });
-        }
-    </script>
-
-    <script src="assets/js/script.js"></script>
-
-
-  </body>
+</body>
 </html>
